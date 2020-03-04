fa687139049ad3500ed068fc67ea319a32789b10
sebsch <sebsch@geblubber.org>
2020-03-04 09:47:43 +0100

    simplified truncate method osing format macro

--- src/data/string_manipulation.rs
+++ src/data/string_manipulation.rs
@@ -1,14 +1,9 @@
 use std::cmp::Ordering;
 
+#[allow(dead_code)]
 pub fn truncate(word: String, length: usize) -> String {
     match word.chars().count().cmp(&length) {
-        Ordering::Greater => {
-            word.chars().collect::<Vec<_>>()[0..(length - 4)]
-                .iter()
-                .cloned()
-                .collect::<String>()
-                + "..."
-        }
+        Ordering::Greater => format!("{:.wlen$} ...", word, wlen = (length - 4)),
         _ => word,
     }
 }
