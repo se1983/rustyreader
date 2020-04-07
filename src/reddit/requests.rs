98cc9b9135c89d0d9c7e29a6c2190472ebc48e6f
sebsch <sebsch@geblubber.org>
2020-04-07 11:06:35 +0200

    Add Cacher

--- src/reddit/requests.rs
+++ src/reddit/requests.rs
@@ -1,32 +1,34 @@
 use ureq;
+use std::collections::HashMap;
+use std::time::{Instant, Duration};
 
 
-struct Cacher<T> where T: Fn(&str) -> String {
-    operation: T,
-    data: Option<String>
+pub struct Cacher<T> where T: Fn(&str) -> String {
+    call: T,
+    data: HashMap<String, (String, Instant)>,
+    cache_time: Duration,
 }
 
-impl<T> Cacher<T> where T: Fn(&str) -> String{
-    fn new(operation: T) -> Self<T> {
-        Self {
-            operation,
-            data: None
+impl<T> Cacher<T> where T: Fn(&str) -> String {
+    pub fn new(call: T) -> Cacher<T> {
+        Cacher {
+            call,
+            data: HashMap::new(),
+            cache_time: Duration::from_millis(10000),
         }
     }
 
+    fn data(&mut self, url: &str) -> String {
+        println!("Calling request {}", &url);
 
-    fn data(&mut self, url: &str, query: &str) -> String {
-        match &self.data {
-            Some(d) => d.clone(),
-            None    => {
-                let response = (self.operation)(&url, &query);
-                let d = response.into_string().unwrap();
-                self.data  = Some(d);
-                d.clone()
+        match self.data.get(url) {
+            Some((data, created)) if created.elapsed() < self.cache_time => data.clone(),
+            _ => {
+                let now = Instant::now();
+                let data = (self.call)(url);
+                self.data.insert(String::from(url), (data.clone(), now));
+                data
             }
         }
     }
 }
-
-
-
