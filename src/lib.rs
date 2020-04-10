56ec5129d608620c1a796653edb8e098ddc21759
sebsch <sebsch@geblubber.org>
2020-04-10 12:10:36 +0200

    Add enumeration

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,9 @@ use reddit::RedditClient;
 
 
 pub fn run() {
-    for listing in RedditClient::new("/r/all").limit(3) {
-        println!("\n\n'{}' has {} comments.\n\t\thttps://reddit.com{}\n\t ",
+    for (i, listing) in RedditClient::new("/r/programming").limit(300).enumerate() {
+        println!("\n\n'[{}]\t{}' has {} comments.\n\t\thttps://reddit.com{}\n\t ",
+                 i,
                  listing.link.data.children[0].data.title,
                  listing.comments.data.children.len(),
                  listing.link.data.children[0].data.permalink,
