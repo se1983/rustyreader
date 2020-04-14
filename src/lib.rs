0dc6056800ea6cceb311127b1b6b04033dbb21d6
sebsch <sebsch@geblubber.org>
2020-04-14 10:07:26 +0200

    Search in comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,15 @@
-mod reddit;
-
 use reddit::RedditClient;
 
+mod reddit;
 
 pub fn run() {
-    for (i, listing) in RedditClient::new("/r/programming").limit(300).enumerate() {
-        println!("\n\n'[{}]\t{}' has {} comments.\n\t\thttps://reddit.com{}\n\t ",
-                 i,
-                 listing.link.data.children[0].data.title,
-                 listing.comments.data.children.len(),
-                 listing.link.data.children[0].data.permalink,
-        );
-
-        listing.comments.data.children.iter().for_each(|comment| {
-            println!("-- {}", match &comment.data.body {
-                Some(body) if false => body.clone(),
-                _ => String::from(""),
-            });
-        })
+    for (_, listing) in RedditClient::new("/r/rust").limit(100).enumerate() {
+        for l in listing.comments.data.children.iter()
+            .into_iter()
+            .filter(|x| x.data.body.is_some())
+            .map(|x| x.data.body.as_ref().unwrap())
+            .filter(|x| x.contains("Rust")) {
+            println!("{}", l);
+        }
     };
 }
