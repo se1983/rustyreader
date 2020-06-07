96dded1d64494c0bd5ce98b7831effe342a2ddb6
sebastian <sebsch@geblubber.org>
2020-06-07 14:44:57 +0200

    Searching any word from list in post

--- src/lib.rs
+++ src/lib.rs
@@ -3,12 +3,15 @@ use reddit::RedditClient;
 mod reddit;
 
 pub fn run() {
+
+    let searchwords = vec!("Rust", "skajsdkahk");
+
     for (_, listing) in RedditClient::new("/r/rust").limit(100).enumerate() {
         for l in listing.comments.data.children.iter()
             .into_iter()
             .filter(|x| x.data.body.is_some())
             .map(|x| x.data.body.as_ref().unwrap())
-            .filter(|x| x.contains("Rust")) {
+            .filter(|x| searchwords.iter().any(|y| x.contains(y))) {
             println!("{}", l);
         }
     };
