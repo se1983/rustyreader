31a5161bd2644ba80d58b6517a2e7edf9973bd7e
sebastian <sebsch@geblubber.org>
2020-06-07 20:42:36 +0200

    parametrize the function call

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,22 @@ use reddit::RedditClient;
 
 mod reddit;
 
+fn do_print(post: &str){
+   println!("------------------------------------------------------------------\n------------------------------------------------------------------\n{}\n\n", post)
+}
+
 pub fn run() {
 
     let searchwords = vec!("Rust", "skajsdkahk");
     let subreddit = "/r/rust";
+    let do_sth = do_print;
 
     for (_, listing) in RedditClient::new(subreddit).limit(100).enumerate() {
         for l in listing.comments.data.children.iter()
             .filter(|x| x.data.body.is_some())
             .map(|x| x.data.body.as_ref().unwrap())
             .filter(|x| searchwords.iter().any(|y| x.contains(y))) {
-            println!("{}", l);
+           do_sth(l);
         }
     };
 }
